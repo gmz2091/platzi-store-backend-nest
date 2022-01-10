ae5d9bb95b7b33b98efbd1cab3813043c17e917b
Magdiel <mgomez2091@gmail.com>
2022-01-10 02:25:22 -0700

    Commenting unnecessary code

--- src/dtos/products.dtos.ts
+++ src/dtos/products.dtos.ts
@@ -1,34 +1,34 @@
-import {
-  IsString,
-  IsNumber,
-  IsUrl,
-  IsNotEmpty,
-  IsPositive,
-} from 'class-validator';
-import { PartialType } from '@nestjs/mapped-types';
+// import {
+//   IsString,
+//   IsNumber,
+//   IsUrl,
+//   IsNotEmpty,
+//   IsPositive,
+// } from 'class-validator';
+// import { PartialType } from '@nestjs/mapped-types';
 
-export class CreateProductDto {
-  @IsString()
-  @IsNotEmpty()
-  readonly name: string;
+// export class CreateProductDto {
+//   @IsString()
+//   @IsNotEmpty()
+//   readonly name: string;
 
-  @IsString()
-  @IsNotEmpty()
-  readonly description: string;
+//   @IsString()
+//   @IsNotEmpty()
+//   readonly description: string;
 
-  @IsNumber()
-  @IsNotEmpty()
-  @IsPositive()
-  readonly price: number;
+//   @IsNumber()
+//   @IsNotEmpty()
+//   @IsPositive()
+//   readonly price: number;
 
-  @IsNumber()
-  @IsNotEmpty()
-  @IsPositive()
-  readonly stock: number;
+//   @IsNumber()
+//   @IsNotEmpty()
+//   @IsPositive()
+//   readonly stock: number;
 
-  @IsUrl()
-  @IsNotEmpty()
-  readonly image: string;
-}
+//   @IsUrl()
+//   @IsNotEmpty()
+//   readonly image: string;
+// }
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+// export class UpdateProductDto extends PartialType(CreateProductDto) {}
