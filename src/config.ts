cedbba273e6164fed29efa006577df14b738fa26
Magdiel <mgomez2091@gmail.com>
2022-01-12 03:10:40 -0700

    Config enviroments

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ export default registerAs('config', () => {
       name: process.env.DATABASE_NAME,
       port: process.env.DATABASE_PORT,
     },
-    postgresURL: process.env.DATABASE_URl,
+    postgresURL: process.env.DATABASE_URL,
     // postgres: {
     //   dbName: process.env.POSTGRES_DB,
     //   port: parseInt(process.env.POSTGRES_PORT, 10),
