c0627af38b5ae33a47269ab89aec569e2634d698
Magdiel <mgomez2091@gmail.com>
2022-01-12 02:26:53 -0700

    Config enviroments 1.2

--- src/config.ts
+++ src/config.ts
@@ -9,13 +9,13 @@ export default registerAs('config', () => {
       port: process.env.DATABASE_PORT,
     },
     postgresURL: process.env.DATABASE_URl,
-    postgres: {
-      dbName: process.env.POSTGRES_DB,
-      port: parseInt(process.env.POSTGRES_PORT, 10),
-      password: process.env.POSTGRES_PASSWORD,
-      user: process.env.POSTGRES_USER,
-      host: process.env.POSTGRES_HOST,
-    },
+    // postgres: {
+    //   dbName: process.env.POSTGRES_DB,
+    //   port: parseInt(process.env.POSTGRES_PORT, 10),
+    //   password: process.env.POSTGRES_PASSWORD,
+    //   user: process.env.POSTGRES_USER,
+    //   host: process.env.POSTGRES_HOST,
+    // },
     apiKey: process.env.API_KEY,
     jwtSecret: process.env.JWT_SECRET,
   };
